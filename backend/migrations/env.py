6df7786b493d8dcadc042f216d10940141affc2c
Jobin <jobinshery@karunya.edu.in>
2026-08-18 15:29:02 +0530

    Fix URL parsing

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -8,7 +8,7 @@ from app import models  # noqa: F401
 
 
 config = context.config
-config.set_main_option("sqlalchemy.url", database_url())
+config.set_main_option("sqlalchemy.url", database_url().replace("%", "%%"))
 if config.config_file_name:
     fileConfig(config.config_file_name)
 target_metadata = Base.metadata
